e51c36110dbd2ceb8ca60878052e759fa9eaed61
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 11:47:39 +0200

    Added IO/SizeInfo plus tests.

---
+++ Source/Kf.Core.Tests/IO/SizeInfoTests.cs
@@ -0,0 +1,90 @@
+﻿using Kf.Core.IO;
+using Xunit;
+
+namespace Kf.Core.Tests.IO
+{
+    public class SizeInfoTests
+    {
+        static long _bytes = 6316621824; // 6024MB
+        static SizeInfo _sut = new SizeInfo(_bytes);
+
+        [Fact]
+        public void Can_Convert_To_KiloBytes() {
+            // Arrange
+            var expected = 6168576;
+
+            // Act            
+            // Assert
+            Assert.Equal(expected, _sut.KiloBytes);
+        }
+
+        [Fact]
+        public void Can_Convert_To_MegaBytes() {
+            // Arrange
+            var expected = 6024d;
+
+            // Act            
+            // Assert
+            Assert.Equal(expected, _sut.MegaBytes);
+        }
+
+        [Fact]
+        public void Can_Convert_To_GigaBytes() {
+            // Arrange
+            var expected = 5.8828125d;
+
+            // Act            
+            // Assert
+            Assert.Equal(expected, _sut.GigaBytes);
+        }
+
+        [Fact]
+        public void Can_Convert_To_TeraBytes() {
+            // Arrange
+            var expected = 0.00574493408203125d;
+
+            // Act            
+            // Assert
+            Assert.Equal(expected, _sut.TeraBytes);
+        }
+
+        [Fact]
+        public void Can_Create_From_GigaBytes() {
+            // Arrange
+            var expected = 27165668147;
+            var gigaBytes = 25.30d;
+
+            // Act
+            var sut = SizeInfo.FromGigaBytes(gigaBytes);
+
+            // Assert
+            Assert.Equal(expected, sut.Bytes);
+        }
+
+        [Fact]
+        public void Can_Create_From_MegaBytes() {
+            // Arrange
+            var expected = 26528973;
+            var megaBytes = 25.30d;
+
+            // Act
+            var sut = SizeInfo.FromMegaBytes(megaBytes);
+
+            // Assert
+            Assert.Equal(expected, sut.Bytes);
+        }
+
+        [Fact]
+        public void Can_Create_From_KiloBytes() {
+            // Arrange
+            var expected = 26528973;
+            var kiloBytes = 25907.2d;
+
+            // Act
+            var sut = SizeInfo.FromKiloBytes(kiloBytes);
+
+            // Assert
+            Assert.Equal(expected, sut.Bytes);
+        }
+    }
+}
---
+++ Source/Kf.Core/IO/SizeInfo.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+
+namespace Kf.Core.IO
+{
+    /// <summary>
+    /// Represents file size information.
+    /// </summary>
+    /// <remarks>
+    /// CONVERSTION TABLE
+    /// -- NAME --------- SYMBOL -------- DECIMAL ----------- #BYTES ---------- EQUALS
+    ///    Bit            b               n/a                 0.125             n/a
+    ///    Nibble         Nibble          n/a                 0.5               4b
+    ///    Byte           B               n/a                 1                 2Nibble
+    ///    Kilobyte       KB              10^3                1024              1024B
+    ///    Megabyte       MB              10^6                1048576           1024KB
+    ///    Gigabyte       GB              10^9                1073741824        1024MB
+    ///    Terabyte       TB              10^12               1099511627776     1024GB    
+    /// </remarks>
+    public class SizeInfo
+    {
+        #region Constants
+        private const long BYTES_IN_KILOBYTE = 1024;
+        private const long BYTES_IN_MEGABYTE = 1048576;
+        private const long BYTES_IN_GIGABYTE = 1073741824;
+        private const long BYTES_IN_TERABYTE = 1099511627776;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the size in Bytes (B).
+        /// </summary>
+        public long Bytes { get { return (long)_bytes; } }
+        private readonly double _bytes;
+
+        /// <summary>
+        /// Gets the size in KiloBytes (KB).
+        /// </summary>
+        public double KiloBytes { get { return _kiloBytes; } }
+        private readonly double _kiloBytes;
+
+        /// <summary>
+        /// Gets the size in MegaBytes (MB).
+        /// </summary>
+        public double MegaBytes { get { return _megaBytes; } }
+        private readonly double _megaBytes;
+
+        /// <summary>
+        /// Gets the size in GigaBytes (GB).
+        /// </summary>
+        public double GigaBytes { get { return _gigaBytes; } }
+        private readonly double _gigaBytes;
+
+        /// <summary>
+        /// Gets the size in TeraBytes (TB).
+        /// </summary>
+        public double TeraBytes { get { return _teraBytes; } }
+        private readonly double _teraBytes;
+        #endregion
+
+        #region Construction
+        /// <summary>
+        /// Creates a new <see cref="SizeInfo"/> object.
+        /// </summary>
+        /// <param name="bytes">The file size in bytes.</param>
+        public SizeInfo(long bytes) {
+            _bytes = bytes;
+            _kiloBytes = _bytes / BYTES_IN_KILOBYTE;
+            _megaBytes = _bytes / BYTES_IN_MEGABYTE;
+            _gigaBytes = _bytes / BYTES_IN_GIGABYTE;
+            _teraBytes = _bytes / BYTES_IN_TERABYTE;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SizeInfo"/> object.
+        /// </summary>
+        /// <param name="memoryStream">The <see cref="MemoryStream"/> object to get the filesize for.</param>
+        /// <exception cref="System.ObjectDisposedException"></exception>
+        public SizeInfo(MemoryStream memoryStream)
+            : this(memoryStream.Length) {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SizeInfo"/> object based on the given <see cref="KiloBytes"/>.
+        /// </summary>
+        /// <param name="kiloBytes">The size in KB.</param>
+        /// <returns>A <see cref="FileSizeInfo"/> object.</returns>
+        public static SizeInfo FromKiloBytes(double kiloBytes) {
+            return new SizeInfo(Convert.ToInt64(Math.Round(kiloBytes * (double)1024, 0)));
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SizeInfo"/> object based on the given <see cref="MegaBytes"/>.
+        /// </summary>
+        /// <param name="megaBytes">The size in MB.</param>
+        /// <returns>A <see cref="FileSizeInfo"/> object.</returns>
+        public static SizeInfo FromMegaBytes(double megaBytes) {
+            return new SizeInfo(Convert.ToInt64(Math.Round(megaBytes * (double)1024 * (double)1024, 0)));
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SizeInfo"/> object based on the given <see cref="GigaBytes"/>.
+        /// </summary>
+        /// <param name="gigaBytes">The size in GB.</param>
+        /// <returns>A <see cref="FileSizeInfo"/> object.</returns>
+        public static SizeInfo FromGigaBytes(double gigaBytes) {
+            return new SizeInfo(Convert.ToInt64(Math.Round(gigaBytes * (double)1024 * (double)1024 * (double)1024, 0)));
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SizeInfo"/> object based on a given <see cref="MemoryStream"/>.
+        /// </summary>
+        /// <param name="memoryStream">The <see cref="MemoryStream"/> object to get the filesize for.</param>
+        /// <exception cref="System.ObjectDisposedException"></exception>
+        /// <returns>A <see cref="FileSizeInfo"/> object.</returns>
+        public static SizeInfo FromMemoryStream(MemoryStream memoryStream) {
+            return new SizeInfo(memoryStream);
+        }
+        #endregion
+    }
+}
