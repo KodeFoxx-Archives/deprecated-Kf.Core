9f14c04a8bf2fb124b9506c6f04eec63bc5bfae5
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 10:44:45 +0200

    Added tests for TypeExtensions (ToFriendlyName).

---
+++ Source/Kf.Core.Tests/(extensions)/System/TypeExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kf.Core.Tests._extensions_.System
+{
+    public class TypeExtensionsTests
+    {
+        [Fact]
+        public void ReturnsTypeNameForSimpleType() {
+            var expected = "DateTime";
+            var sut = typeof(DateTime);
+            var actual = sut.ToFriendlyName();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnsTypeNameForGenericType() {
+            var expected = "List<String>";
+            var sut = typeof(List<string>);
+            var actual = sut.ToFriendlyName();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnsTypeNameForComplexGenericType() {
+            var expected = "IDictionary<DateTime, List<String>>";
+            var sut = typeof(IDictionary<DateTime, List<string>>);
+            var actual = sut.ToFriendlyName();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
