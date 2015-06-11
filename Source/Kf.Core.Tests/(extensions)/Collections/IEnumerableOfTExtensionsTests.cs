6e7d7a04b3b1efbe00d124bdcd6fd827af929637
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 13:17:26 +0200

    Added .ForEach<TIn, TOut>() construct/extension method and tests.

--- Source/Kf.Core.Tests/(extensions)/Collections/IEnumerableOfTExtensionsTests.cs
+++ Source/Kf.Core.Tests/(extensions)/Collections/IEnumerableOfTExtensionsTests.cs
@@ -22,5 +22,27 @@ namespace Kf.Core.Tests._extensions_.Collections
                 index++;
             }
         }
+
+        [Fact]
+        public void ForEachPerformsCorrectAlgorithm() {
+            var sut = Enumerable.Range(0, 500);
+            var result = sut.ForEach(i => { return 0; });
+            Assert.True(result.All(i => i == 0));
+        }
+
+        [Fact]
+        public void ForEachCanReturnDifferentType() {
+            var sut = Enumerable.Range(0, 500);
+            var result = sut.ForEach(i => { return i.ToString("000"); });
+            Assert.True(result.All(i => i.Length.Equals(3)));
+        }
+
+        [Fact]
+        public void ForEachCanReturnAndCOntinueLinqStyle() {
+            var sut = Enumerable.Range(0, 500);
+            var result = sut.ForEach(i => { return i.ToString("000"); });
+            result = result.Where(x => x.Contains("449"));
+            Assert.True(result.Count().Equals(1));
+        }
     }
 }
--- Source/Kf.Core/(extensions)/Collections/IEnumerableOfTExtensions.cs
+++ Source/Kf.Core/(extensions)/Collections/IEnumerableOfTExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Kf.Core.Helpers.ForEach;
 
 namespace Kf
@@ -13,6 +14,20 @@ namespace Kf
         /// <returns>IEnumerable<ForEachWrapper<TValue, int>></returns>
         public static IEnumerable<ForEachItem<TValue, int>> Index<TValue>(this IEnumerable<TValue> enumerable) {
             return ForEachHelper.Index<TValue>(enumerable);
+        }
+
+        /// <summary>
+        /// Construct to do a manipulation on each item in an enumerable and continue working on it afterwards.
+        /// </summary>
+        /// <typeparam name="TIn">The type of items in the enumerable</typeparam>
+        /// <typeparam name="TOut">The type of items the algorithm needs to return</typeparam>
+        /// <param name="enumerable">The enumerable to apply the foreach to.</param>
+        /// <param name="algorithm">The algorithm to execute on each item in the enumerable</param>
+        /// <returns>The <see cref="IEnumerable{TOut}"/> with the manipulations done.</returns>
+        public static IEnumerable<TOut> ForEach<TIn, TOut>(this IEnumerable<TIn> enumerable, Func<TIn,TOut> algorithm) {
+            foreach (var item in enumerable) {
+                yield return algorithm(item);
+            }
         }
     }
 }
