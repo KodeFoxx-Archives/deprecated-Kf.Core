95be0bcb3614ed5ca1875ca827bb6bb95d79207c
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 11:50:19 +0200

    Added extensions method for MemoryStream to get "SizeInfo".

---
+++ Source/Kf.Core/(extensions)/System/IO/MemoryStreamExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Kf.Core.IO;
+
+namespace Kf
+{
+    /// <summary>
+    /// Holds extension methods for use with <see cref="MemoryStream"/> objects.
+    /// </summary>
+    public static class MemoryStreamExtensions
+    {
+        /// <summary>
+        /// Gets the size as a <see cref="SizeInfo"/>.
+        /// </summary>
+        /// <param name="memoryStream">The <see cref="MemoryStream"/> object to get the size for.</param>
+        /// <returns>A <see cref="SizeInfo"/> object representing the <see cref="MemoryStream"/>'s size.</returns>
+        public static SizeInfo GetSizeInfo(this MemoryStream memoryStream) {
+            return SizeInfo.FromMemoryStream(memoryStream);
+        }
+    }
+}
