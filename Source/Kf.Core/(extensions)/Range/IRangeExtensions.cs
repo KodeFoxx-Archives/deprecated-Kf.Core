b9047ead5ea12bb18c00f5ec4aadc3b0a5b9bd5b
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 10:17:19 +0200

    Added IRangeExtensions and DateTimeExtensions.

---
+++ Source/Kf.Core/(extensions)/Range/IRangeExtensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kf.Core.Range;
+
+namespace Kf
+{
+    /// <summary>
+    /// Provides extension methods to interact with <see cref="IRange"/> of "T" objects.
+    /// </summary>
+    public static class IRangeExtensions
+    {
+        #region IRange<DateTime>
+        /// <summary>
+        /// Lists all weekdays of an <see cref="IRange"/> of <see cref="DateTime"/> in an <see cref="IEnumerable"/>.
+        /// </summary>        
+        /// <returns>An <see cref="IEnumerable"/> of <see cref="int"> containing all weekdays within the specified range.</returns>
+        public static IEnumerable<DateTime> GetWeekdays(this IRange<DateTime> range) {
+            return range.GetDays(d => !d.IsWeekend());
+        }
+
+        /// <summary>
+        /// Lists all weekend days of an <see cref="IRange"/> of <see cref="DateTime"/> in an <see cref="IEnumerable"/>.
+        /// </summary>        
+        /// <returns>An <see cref="IEnumerable"/> of <see cref="int"> containing all weekend days within the specified range.</returns>
+        public static IEnumerable<DateTime> GetWeekendDays(this IRange<DateTime> range) {
+            return range.GetDays(d => d.IsWeekend());
+        }
+
+        /// <summary>
+        /// Lists all <see cref="Datetime"/> objects of an <see cref="IRange"/> of <see cref="Datetime"/> that are equal to the given <see cref="DayOfWeek"/>.
+        /// </summary>        
+        /// <param name="dayOfWeek">The <see cref="DayOfWeek"/> to return all days of.</param>
+        /// <returns>All <see cref="Datetime"/> objects of an <see cref="IRange"/> of <see cref="Datetime"/> that are equal to the given <see cref="DayOfWeek"/>.</returns>
+        public static IEnumerable<DateTime> GetDays(this IRange<DateTime> range, DayOfWeek dayOfWeek) {
+            return range.GetDays(d => d.DayOfWeek.Equals(dayOfWeek));
+        }
+
+        /// <summary>
+        /// Lists all <see cref="Datetime"/> objects of an <see cref="IRange"/> of <see cref="Datetime"/> that are equal to the given <see cref="DayOfWeek"/>s.
+        /// </summary>        
+        /// <param name="daysOfWeek">The <see cref="DayOfWeek"/>s to return all days of.</param>
+        /// <returns>All <see cref="Datetime"/> objects of an <see cref="IRange"/> of <see cref="Datetime"/> that are equal to the given <see cref="DayOfWeek"/>s.</returns>
+        public static IEnumerable<DateTime> GetDays(this IRange<DateTime> range, IEnumerable<DayOfWeek> daysOfWeek) {
+            if (daysOfWeek == null) {
+                return new List<DateTime>();
+            }
+
+            return range.GetDays(d => daysOfWeek.Distinct().Contains(d.DayOfWeek));
+        }
+
+        /// <summary>
+        /// Lists all <see cref="DateTime"/> objects of an <see cref="IRange"/> of <see cref="DateTime"/> that comply with the given <paramref name="condition"/>.
+        /// </summary>        
+        /// <param name="condition">The condition each date time is tested against.</param>
+        /// <returns>All <see cref="DateTime"/> objects of an <see cref="IRange"/> of <see cref="DateTime"/> that comply with the given <paramref name="condition"/>.</returns>
+        public static IEnumerable<DateTime> GetDays(this IRange<DateTime> range, Func<DateTime, bool> condition) {
+            var date = range.Minimum;
+
+            do {
+                if (condition(date)) {
+                    yield return date.Date;
+                }
+
+                date = date.NextDay();
+            } while (range.IsInRange(date));
+        }
+
+        /// <summary>
+        /// Lists all <see cref="DateTime"/> objects of an <see cref="IRange"/> of <see cref="DateTime"/>.
+        /// </summary>
+        /// <returns>All <see cref="DateTime"/> objects of an <see cref="IRange"/> of <see cref="DateTime"/>.</returns>
+        public static IEnumerable<DateTime> GetDays(this IRange<DateTime> range) {
+            var date = range.Minimum;
+
+            do {
+                yield return date.Date;
+
+                date = date.NextDay();
+            } while (range.IsInRange(date));
+        }
+        #endregion
+    }
+}
---
+++ Source/Kf.Core/(extensions)/System/DateTimeExtensions.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kf.Core.Range;
+
+namespace Kf
+{
+    /// <summary>
+    /// Provides extension methods to interact with <see cref="DateTime"/> objects.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        #region IsDay (Method Family)
+        /// <summary>
+        /// Holds a reference to the weekend days.
+        /// </summary>
+        private readonly static IEnumerable<DayOfWeek> _weekendDays = new List<DayOfWeek>() {
+            DayOfWeek.Saturday, DayOfWeek.Sunday
+        };
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> matches with the given <paramref name="dayOfWeek"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <param name="dayOfWeek">The <see cref="DayOfWeek"/> to check for.</param>
+        /// <returns>True when <paramref name="dayOfWeek"/> matches; false when not.</returns>
+        public static bool IsDay(this DateTime dateTime, DayOfWeek dayOfWeek) {
+            return dateTime.DayOfWeek.Equals(dayOfWeek);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> matches with any of the given <paramref name="daysOfWeek"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <param name="daysOfWeek">The <see cref="DayOfWeek"/>s to check for.</param>
+        /// <returns>True when <paramref name="daysOfWeek"/> matches one; false when not.</returns>
+        public static bool IsDayIn(this DateTime dateTime, IEnumerable<DayOfWeek> daysOfWeek) {
+            if (daysOfWeek == null)
+                return false;
+
+            if (daysOfWeek.Count() == 0)
+                return false;
+
+            return daysOfWeek.Contains(dateTime.DayOfWeek);
+        }
+
+        /// <summary>
+        /// Datermines whether the current <see cref="DateTime"/> is a weekend day (<see cref="DayOfWeek.Saturday"/> or <see cref="DayOfWeek.Sunday"/>).
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when weekend day (<see cref="DayOfWeek.Saturday"/> or <see cref="DayOfWeek.Sunday"/>); false when not.</returns>
+        public static bool IsWeekend(this DateTime dateTime) {
+            return _weekendDays.Contains(dateTime.DayOfWeek);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> is a <see cref="DayOfWeek.Monday"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when <see cref="DayOfWeek.Monday"/>, false when not.</returns>
+        public static bool IsMonday(this DateTime dateTime) {
+            return dateTime.IsDay(DayOfWeek.Monday);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> is a <see cref="DayOfWeek.Tuesday"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when <see cref="DayOfWeek.Tuesday"/>, false when not.</returns>
+        public static bool IsTuesday(this DateTime dateTime) {
+            return dateTime.IsDay(DayOfWeek.Tuesday);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> is a <see cref="DayOfWeek.Wednesday"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when <see cref="DayOfWeek.Wednesday"/>, false when not.</returns>
+        public static bool IsWednesday(this DateTime dateTime) {
+            return dateTime.IsDay(DayOfWeek.Wednesday);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> is a <see cref="DayOfWeek.Thursday"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when <see cref="DayOfWeek.Thursday"/>, false when not.</returns>
+        public static bool IsThursday(this DateTime dateTime) {
+            return dateTime.IsDay(DayOfWeek.Thursday);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> is a <see cref="DayOfWeek.Friday"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when <see cref="DayOfWeek.Friday"/>, false when not.</returns>
+        public static bool IsFriday(this DateTime dateTime) {
+            return dateTime.IsDay(DayOfWeek.Friday);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> is a <see cref="DayOfWeek.Saturday"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when <see cref="DayOfWeek.Saturday"/>, false when not.</returns>
+        public static bool IsSaturday(this DateTime dateTime) {
+            return dateTime.IsDay(DayOfWeek.Saturday);
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="DateTime"/> is a <see cref="DayOfWeek.Sunday"/>.
+        /// </summary>
+        /// <param name="dateTime">The given date to check.</param>
+        /// <returns>True when <see cref="DayOfWeek.Sunday"/>, false when not.</returns>
+        public static bool IsSunday(this DateTime dateTime) {
+            return dateTime.IsDay(DayOfWeek.Sunday);
+        }
+        #endregion
+
+        #region NextDay (Method Family)
+        /// <summary>
+        /// Holds a reference to the range of "DayOfWeek".
+        /// </summary>
+        private readonly static IRange<int> _dayOfWeekRange = new Int32Range(0, 6);
+
+        /// <summary>
+        /// Returns the next day for the current <see cref="DateTime"/>.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> to return the next day for.</param>
+        /// <returns>The next day for the current <see cref="DateTime"/>.</returns>
+        public static DateTime NextDay(this DateTime dateTime) {
+            return dateTime.AddDays(1);
+        }
+
+        /// <summary>
+        /// Returns the previous day for the current <see cref="DateTime"/>.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> to return the previous day for.</param>
+        /// <returns>The previous day for the current <see cref="DateTime"/>.</returns>
+        public static DateTime PreviousDay(this DateTime dateTime) {
+            return dateTime.AddDays(-1);
+        }
+
+        /// <summary>
+        /// Returns the next <paramref name="dayOfWeek"/> starting from the current <see cref="Datetime"/>.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> the calculation starts from.</param>
+        /// <param name="dayOfWeek">The <see cref="DayOfWeek"/> to get.</param>
+        /// <returns>The next <paramref name="dayOfWeek"/> starting from the current <see cref="Datetime"/>.</returns>
+        public static DateTime NextDay(this DateTime dateTime, DayOfWeek dayOfWeek) {
+            var addDaysModifier = CalculateDifferenceBetweenDayOfWeek(dateTime.DayOfWeek, dayOfWeek);
+            if (addDaysModifier == 0) {
+                addDaysModifier = 7;
+            }
+
+            return dateTime.AddDays(addDaysModifier);
+        }
+
+        /// <summary>
+        /// Returns the previous <paramref name="dayOfWeek"/> starting from the current <see cref="Datetime"/>.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> the calculation starts from.</param>
+        /// <param name="dayOfWeek">The <see cref="DayOfWeek"/> to get.</param>
+        /// <returns>The previous <paramref name="dayOfWeek"/> starting from the current <see cref="Datetime"/>.</returns>
+        public static DateTime PreviousDay(this DateTime dateTime, DayOfWeek dayOfWeek) {
+            var addDaysModifier = CalculateDifferenceBetweenDayOfWeek(dayOfWeek, dateTime.DayOfWeek) * -1;
+            if (addDaysModifier == 0) {
+                addDaysModifier = -7;
+            }
+
+            return dateTime.AddDays(addDaysModifier);
+        }
+
+        /// <summary>
+        /// Calculates the difference between two <see cref="DayOfWeek"/>.
+        /// </summary>
+        /// <remarks>
+        /// Props to Tim Schmelter and mbeckish @[http://stackoverflow.com/questions/9219083/get-difference-in-days-between-two-weekdays]
+        /// </remarks>
+        /// <param name="start">Starting <see cref="DayOfWeek"/>.</param>
+        /// <param name="end">Ending <see cref="DayOfWeek"/>.</param>
+        /// <returns>The difference between two <see cref="DayOfWeek"/>s.</returns>
+        private static int CalculateDifferenceBetweenDayOfWeek(DayOfWeek start, DayOfWeek end) {
+            return (7 + (end - start)) % 7;
+        }
+        #endregion
+    }
+}
