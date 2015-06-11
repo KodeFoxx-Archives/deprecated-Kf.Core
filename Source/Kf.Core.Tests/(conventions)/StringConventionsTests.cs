e6f370619aa0e4ad1e67df192ca06fc05b42b8ba
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 09:21:28 +0200

    Added tests for StringConventions FMT_NULL and FMT_EXCEPTION.

---
+++ Source/Kf.Core.Tests/(conventions)/StringConventionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Reflection;
+using Kf.Core.Conventions;
+using Xunit;
+
+namespace Kf.Core.Tests.Conventions
+{
+    public class StringConventionsTests
+    {
+        private FieldInfo FindStaticFieldInStringConventions(string fieldName) {
+            return typeof(StringConventions)
+                .GetFields()            
+                .Where(m => m.Name.Equals(fieldName))
+                .SingleOrDefault();
+        }
+
+        [Fact]
+        public void NullStringIsDefinedByNameFMT_NULL() {
+            var expected = "FMT_NULL";
+            var actual = FindStaticFieldInStringConventions(expected);
+            Assert.NotNull(actual);            
+        }
+
+        [Fact]
+        public void NullStringContainsCorrectValue() {
+            var expected = "(*null)";
+            var sut = FindStaticFieldInStringConventions("FMT_NULL");
+            var actual = sut.GetValue(null);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ExceptionStringIsDefinedByNameFMT_EXCEPTION() {
+            var expected = "FMT_EXCEPTION";
+            var actual = FindStaticFieldInStringConventions(expected);
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void ExceptionStringContainsCorrectValue() {
+            var expected = "(*ex!{0})";
+            var sut = FindStaticFieldInStringConventions("FMT_EXCEPTION");
+            var actual = sut.GetValue(null);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
