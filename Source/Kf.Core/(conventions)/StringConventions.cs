a5fedf83a33ebd9adc60f64cb8a6150a21910309
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 08:54:40 +0200

    Added "StringConventions".

---
+++ Source/Kf.Core/(conventions)/StringConventions.cs
@@ -0,0 +1,21 @@
+﻿namespace Kf.Core.Conventions
+{
+    /// <summary>
+    /// Holds common used strings.
+    /// </summary>
+    public static class StringConventions
+    {
+        /// <summary>
+        /// Represents a null value.
+        /// Value: (*null)
+        /// </summary>
+        public static readonly string FMT_NULL = "(*null)";
+
+        /// <summary>
+        /// Represents an exception value.
+        /// Acceps a one argument formatting parameter.
+        /// Value: (*ex!{0})
+        /// </summary>
+        public static readonly string FMT_EXCEPTION = "(*ex!{0})";
+    }
+}
