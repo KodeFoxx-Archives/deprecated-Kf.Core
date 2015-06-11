fce29161d264866b4b400e7c7c0792147b5e089e
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 11:12:15 +0200

    Added Exceptions to be used when something goes wrong for Timestamping.

---
+++ Source/Kf.Core/(conventions)/Timestamping/Exceptions/TimestampException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Kf.Core.Conventions.Timestamping.Exceptions
+{
+    public class TimestampException : Exception
+    {
+        public TimestampException(string message) : base(message) { }
+        public TimestampException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ Source/Kf.Core/(conventions)/Timestamping/Exceptions/UnknownTimestampFormatException.cs
@@ -0,0 +1,13 @@
+﻿namespace Kf.Core.Conventions.Timestamping.Exceptions
+{
+    public sealed class UnknownTimestampFormatException : TimestampException
+    {
+        public UnknownTimestampFormatException(TimestampFormat timestampFormat) :
+            base($"No format defined with name '{timestampFormat.ToString()}'.") {
+        }
+
+        public UnknownTimestampFormatException(string message) :
+            base(message) {
+        }
+    }
+}
