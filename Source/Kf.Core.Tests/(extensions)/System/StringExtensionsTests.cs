6b236d629d18ffa8b829988626c70cd0824fcf75
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 11:04:28 +0200

    Extended StringExtensions with functionality and added tests.

---
+++ Source/Kf.Core.Tests/(extensions)/System/StringExtensionsTests.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using Xunit;
+
+namespace Kf.Core.Tests._extensions_.System
+{
+    public class StringExtensionsTests
+    {
+        #region Manipulation - RemoveMultipleWhiteSpaces()
+        [Fact]
+        public void RemoveMultipleWhitespaces_Returns_Correct_String() {
+            // Arrange
+            var expected = "This string is cleaned from whitespace overload.";
+            var whitespaceOverload = "This            string is cleaned   from   whitespace overload.";
+            var actual = String.Empty;
+
+            // Act
+            actual = whitespaceOverload.RemoveMultipleWhitespaces();
+
+            // Assert
+            Assert.Equal<string>(expected, actual);
+        }
+
+        [Fact]
+        public void RemoveMultipleWhitespaces_Doesnt_Throw_Exception_On_Null_String() {
+            // Arrange
+            var expected = (Exception)null;
+            var actual = (Exception)null;
+            var nullString = (string)null;
+
+            // Act            
+            try {
+                var whitespacesRemoved = nullString.RemoveMultipleWhitespaces();
+            } catch (Exception ex) {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal<Exception>(expected, actual);
+        }
+        #endregion
+
+        #region Manipulation - Reverse()
+        [Fact]
+        public void Reverse_Returs_Correct_String() {
+            // Arrange
+            var input = "This string will be reversed! 007 will never know ;)...";
+            var expected = "...); wonk reven lliw 700 !desrever eb lliw gnirts sihT";
+
+            // Act
+            var actual = input.Reverse();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Reverse_Doesnt_Throw_Exception_On_Null_String() {
+            // Arrange
+            var expected = (Exception)null;
+            var actual = (Exception)null;
+            var nullString = (string)null;
+
+            // Act            
+            try {
+                var reversed = nullString.Reverse();
+            } catch (Exception ex) {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal<Exception>(expected, actual);
+        }
+        #endregion
+
+        #region Substring - GetFirst()
+        [Fact]
+        public void GetFirst_Returns_Correct_String() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "Guinae";
+
+            // Act
+            var actual = input.GetFirst(6);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetFirst_Returns_Full_String_When_Requested_Amount_Exceeds_String_Length() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "Guinae pig.";
+
+            // Act
+            var actual = input.GetFirst(input.Length * 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetFirst_Returns_Empty_String_When_Requested_0_Amount() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = String.Empty;
+
+            // Act
+            var actual = input.GetFirst(0);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetFirst_Returns_Empty_String_When_Requested_Negative_Amount() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = String.Empty;
+
+            // Act
+            var actual = input.GetFirst(-1);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetFirst_Doesnt_Throw_Exception_On_Null_String() {
+            // Arrange
+            var expected = (Exception)null;
+            var actual = (Exception)null;
+            var nullString = (string)null;
+
+            // Act            
+            try {
+                var reversed = nullString.GetFirst(5);
+            } catch (Exception ex) {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal<Exception>(expected, actual);
+        }
+        #endregion
+
+        #region Substring - GetLast()
+        [Fact]
+        public void GetLast_Returns_Correct_String() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "e pig.";
+
+            // Act
+            var actual = input.GetLast(6);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetLast_Returns_Full_String_When_Requested_Amount_Exceeds_String_Length() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "Guinae pig.";
+
+            // Act
+            var actual = input.GetLast(input.Length * 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetLast_Returns_Empty_String_When_Requested_0_Amount() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = String.Empty;
+
+            // Act
+            var actual = input.GetLast(0);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetLast_Returns_Empty_String_When_Requested_Negative_Amount() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = String.Empty;
+
+            // Act
+            var actual = input.GetLast(-1);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetLast_Doesnt_Throw_Exception_On_Null_String() {
+            // Arrange
+            var expected = (Exception)null;
+            var actual = (Exception)null;
+            var nullString = (string)null;
+
+            // Act            
+            try {
+                var reversed = nullString.GetLast(5);
+            } catch (Exception ex) {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal<Exception>(expected, actual);
+        }
+        #endregion
+
+        #region Substring - SubstringSafe()
+        [Fact]
+        public void SubstringSafe_Returns_Correct_String() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "nae";
+
+            // Act
+            var actual = input.SubstringSafe(3, 3);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringSafe_Returns_Empty_String_When_StartIndex_Exceeds_String_Length() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "";
+
+            // Act
+            var actual = input.SubstringSafe(input.Length * 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringSafe_Returns_Full_String_When_StartIndex_Is_Negative_String_Length() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "Guinae pig.";
+
+            // Act
+            var actual = input.SubstringSafe(input.Length * -1);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringSafe_Returns_Full_String_When_Length_Exceeds_String_Length() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "Guinae pig.";
+
+            // Act
+            var actual = input.SubstringSafe(0, input.Length * 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringSafe_Returns_Cubstring_String_When_Length_Exceeds_String_Length() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "e pig.";
+
+            // Act
+            var actual = input.SubstringSafe(5, input.Length * 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringSafe_Returns_Empty_String_When_Length_Is_Negative_String_Length() {
+            // Arrange            
+            var input = "Guinae pig.";
+            var expected = "";
+
+            // Act
+            var actual = input.SubstringSafe(0, input.Length * -1);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubstringSafe_Doesnt_Throw_Exception_On_Null_String() {
+            // Arrange
+            var expected = (Exception)null;
+            var actual = (Exception)null;
+            var nullString = (string)null;
+
+            // Act            
+            try {
+                var reversed = nullString.SubstringSafe(5);
+            } catch (Exception ex) {
+                actual = ex;
+            }
+
+            // Assert
+            Assert.Equal<Exception>(expected, actual);
+        }
+        #endregion
+    }
+}
--- Source/Kf.Core/(extensions)/System/StringExtensions.cs
+++ Source/Kf.Core/(extensions)/System/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Kf
 {
@@ -35,6 +36,139 @@ namespace Kf
         /// </remarks>        
         public static string FormatWith(this string format, IFormatProvider formatProvider, params object[] args) {
             return String.Format(formatProvider, format, args);
+        }
+        #endregion
+
+        #region Manipulation
+        /// <summary>
+        /// Removes all multiple whitespace characters, and replaces them with only one whitespace character.
+        /// </summary>
+        /// <param name="string">The string to remove multiple whitespaces from.</param>
+        /// <returns>The given string with multiple whitespace between other characters removed, and replaced with only one whitespace character.</returns>
+        public static string RemoveMultipleWhitespaces(this string @string) {
+            if (@string == null) {
+                return @string;
+            }
+
+            var cleanedString =
+                Regex
+                .Replace(@string, @"(\s)\s+", "$1");
+
+            return cleanedString.Trim();
+        }
+
+        /// <summary>
+        /// Reverses the given string.
+        /// </summary>
+        /// <param name="string">The string to be reversed.</param>
+        /// <returns>The given string, with all it's characters in reversed order.</returns>
+        public static string Reverse(this string @string) {
+            if (@string == null) {
+                return @string;
+            }
+
+            var characters = @string.ToCharArray();
+            Array.Reverse(characters);
+
+            return new String(characters);
+        }
+        #endregion
+
+        #region Substring
+        /// <summary>
+        /// Gets the first x-<paramref name="amount"/> of characters from the given string.
+        /// If the given string's length is smaller than the requested <see cref="amount"/> the full string is returned.
+        /// If the given <paramref name="amount"/> is negative, an empty string will be returned.
+        /// </summary>
+        /// <param name="string">The string from which to extract the first x-<paramref name="amount"/> of characters.</param>
+        /// <param name="amount">The amount of characters to return.</param>
+        /// <returns>The first x-<paramref name="amount"/> of characters from the given string.</returns>
+        public static string GetFirst(this string @string, int amount) {
+            if (@string == null) {
+                return @string;
+            }
+
+            if (amount < 0) {
+                return String.Empty;
+            }
+
+            if (amount >= @string.Length) {
+                return @string;
+            } else {
+                return @string.Substring(0, amount);
+            }
+
+        }
+
+        /// <summary>
+        /// Gets the last x-<paramref name="amount"/> of characters from the given string.
+        /// If the given string's length is smaller than the requested <see cref="amount"/> the full string is returned.
+        /// If the given <paramref name="amount"/> is negative, an empty string will be returned.
+        /// </summary>
+        /// <param name="string">The string from which to extract the last x-<paramref name="amount"/> of characters.</param>
+        /// <param name="amount">The amount of characters to return.</param>
+        /// <returns>The last x-<paramref name="amount"/> of characters from the given string.</returns>
+        public static string GetLast(this string @string, int amount) {
+            if (@string == null) {
+                return @string;
+            }
+
+            if (amount < 0) {
+                return String.Empty;
+            }
+
+            if (amount >= @string.Length) {
+                return @string;
+            } else {
+                return @string.Substring(@string.Length - amount);
+            }
+        }
+
+        /// <summary>
+        /// A method that wraps Substring() so that it doesn't throw errors when amounts exceed.
+        /// </summary>
+        /// <param name="string"></param>
+        /// <param name="startIndex">The zero-based starting chracter position of a substring in this instance.</param>
+        /// <param name="length">The number of characters in the substring.</param>
+        /// <returns></returns>
+        public static string SubstringSafe(this string @string, int startIndex, int? length = null) {
+            if (@string == null) {
+                return @string;
+            }
+
+            // 01. Normalise input parameters
+            if (startIndex < 0) {
+                startIndex = 0;
+            }
+
+            if (startIndex > @string.Length) {
+                startIndex = @string.Length;
+            }
+
+            if (length != null && length.Value > @string.Length) {
+                length = @string.Length;
+            }
+
+            if (length != null && length.Value < 0) {
+                length = 0;
+            }
+
+            // 02. After normalisation, 
+            // -> make sure startindex + length is not greater than string's length
+            if (length != null && (length.Value + startIndex) > @string.Length) {
+                var overflow = (length.Value + startIndex) - @string.Length;
+                length = length - overflow;
+                if (length < 0) {
+                    length = 0;
+                }
+            }
+
+            // 03. Return value
+            if (length == null) {
+                return @string.Substring(startIndex);
+            } else {
+                return @string.Substring(startIndex, length.Value);
+            }
         }
         #endregion
     }
